2720f6be3286365a56f8d2dae149c0951e14446e
Cibi Kulandaisamy <cibikulandaisamy@Cibi-Kulandaisamy.local>
2018-11-11 17:53:28 +0530

    update

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi Pixel-Pushers</h1>
+    <p>Welcome to learning React.</p>
+    <Link to="/page-2/">Page Numero Duo</Lin>
+  </div>
+)
+
+export default IndexPage
